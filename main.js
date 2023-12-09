d8b77ceadcdca277442f4c52715a5edb853a0309
ksenia <kseniashevelevaa@gmail.com>
2023-12-10 02:18:17 +0300

    banner

--- main.js
+++ main.js
@@ -5,5 +5,7 @@ import './src/components/typography/typography';
 import './src/components/grid/grid';
 import './src/components/app-header/app-header';
 import './src/components/app-logo/app-logo';
+import './src/components/buttons/buttons';
+import './src/components/banner/banner';
 
 import './src/global/util-classes.scss';
