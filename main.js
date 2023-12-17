412f02876e149c0d38ec4c5f8563327341ee264f
ksenia <kseniashevelevaa@gmail.com>
2023-12-17 23:21:40 +0300

    section-1

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 import 'reset-css';
-import './main.scss';
 
 import './src/components/typography/typography';
+import './main.scss';
+
 import './src/components/grid/grid';
 import './src/components/app-header/app-header';
 import './src/components/app-logo/app-logo';
 import './src/components/buttons/buttons';
 import './src/components/banner/banner';
+import './src/components/info/info';
 
-import './src/global/util-classes.scss';
