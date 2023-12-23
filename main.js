eb45b5930f89cec275921387c5482f03fa221839
ksenia <kseniashevelevaa@gmail.com>
2023-12-23 19:03:46 +0300

    made mobile header

--- main.js
+++ main.js
@@ -16,4 +16,5 @@ import './src/components/price/price';
 import './src/components/application/application';
 import './src/components/input/input';
 import './src/components/footer/footer';
+import './src/components/mobile-header/mobile-header';
 
